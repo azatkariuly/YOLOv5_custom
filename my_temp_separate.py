bab1f869a3f2c4cb322701d44355e7916a086e2d
azatkariuly <azat.kariuly@gmail.com>
2023-04-04 13:57:54 +0900

    added seperate highlighting

--- my_temp_separate.py
+++ my_temp_separate.py
@@ -91,7 +91,7 @@ def main():
                     print('Score detected..')
 
                     # save_path = str(Path(str(len(vid_writer) + 1)).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-                    vid_writer.append(cv2.VideoWriter(str(len(vid_writer) + 1) + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h)))
+                    vid_writer.append(cv2.VideoWriter(str(len(vid_writer) + 1) + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height)))
 
                     # write from queue buffer
                     for k in queue:
